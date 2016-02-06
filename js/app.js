54121d1e4925a9f7fc7fadd0433688017239ce8f
Christopher <flaminnutz@gmail.com>
2016-02-05 17:09:24 -0700

    Placed angular app into var
    Angular App was placed into a var for best practice

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-angular.module('hots', [])
-.controller('mainCtrl', function($scope, $http){
+var hots = angular.module('hots', []);
+hots.controller('mainCtrl', function($scope, $http){
     $http.get('json/heroes.json')
         .success(function(data){
             $scope.heroes = data;
