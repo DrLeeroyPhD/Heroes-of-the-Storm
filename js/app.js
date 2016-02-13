c20da5342f0012729fb44b63ae42687c966e661b
Christopher <flaminnutz@gmail.com>
2016-02-13 15:36:01 -0700

    IMPORTANT JSON UPDATE
    Changed http.get request to pull from a live updated JSON rather than a
    locally stored file. Mild styling changes.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var hots = angular.module('hots', []);
 hots.controller('mainCtrl', function($scope, $http){
-    $http.get('json/heroes.json')
+    $http.get('http://heroesjson.com/heroes.json')
         .success(function(data){
             $scope.heroes = data;
     });
